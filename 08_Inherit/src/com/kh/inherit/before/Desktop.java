ef60e63c1aa925eefc8361cd0e1ee9f7e2df2cdd
SkyWithLab <sxsx1354@gmail.com>
2025-09-02 15:14:10 +0900

    Add files via upload

---
+++ 08_Inherit/src/com/kh/inherit/before/Desktop.java
@@ -0,0 +1,66 @@
+package com.kh.inherit.before;
+
+public class Desktop {
+	private String brand;
+	private String code;
+	private String name;
+	private int price;
+	private boolean allInOne;
+	
+	public Desktop( String brand, String code, String name, int price, boolean allInOne){
+		this.brand=brand;
+		this.code=code;
+		this.name=name;
+		this.price=price;
+		this.allInOne=allInOne;
+		
+	}
+	
+	//getter setter -> alt+shift+s 누르고 r
+
+	public String information() {
+		return String.format("brand: %s, code: %s, name: %s, price: %d, allInOne: %b",brand,code,name,price,allInOne);
+		//* String.format(포맷,...):String
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public boolean isAllInOne() { //getter
+		return allInOne;
+	}
+
+	public void setAllInOne(boolean allInOne) {
+		this.allInOne = allInOne;
+	}
+	
+}
